81b2156bfb13193e847ac01777cf70ff63b4476f
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-10-30 19:04:20 +0100

    fix interceptor class

--- src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
@@ -2,22 +2,23 @@ package com.bolsadeideas.springboot.horariointerceptor.app.interceptors;
 
 import java.util.Calendar;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-
-@Component
+@Component("horario")
 public class HorarioInterceptor implements HandlerInterceptor {
-	
 	@Value("${config.horario.apertura}")
 	private Integer apertura;
+	
 	@Value("${config.horario.cierre}")
 	private Integer cierre;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -27,15 +28,15 @@ public class HorarioInterceptor implements HandlerInterceptor {
 		
 		if(hora >= apertura && hora < cierre) {
 			
-			StringBuilder mensaje = new StringBuilder("Bienvenido horario de atención a clientes");
-			mensaje.append(", antendemos desde las ");
+			StringBuilder mensaje = new StringBuilder("Bienvenidos al horario de atencion a clientes");
+			mensaje.append(", atendemos desde las ");
 			mensaje.append(apertura);
 			mensaje.append("hrs. ");
 			mensaje.append("hasta las ");
 			mensaje.append(cierre);
-			mensaje.append(". Gracias por su visita. ");
+			mensaje.append("hrs. ");
+			mensaje.append("Gracias por su visita.");
 			request.setAttribute("mensaje", mensaje.toString());
-			
 			return true;
 		}
 		response.sendRedirect(request.getContextPath().concat("/cerrado"));
@@ -45,11 +46,11 @@ public class HorarioInterceptor implements HandlerInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		
 		String mensaje = (String) request.getAttribute("mensaje");
-		modelAndView.addObject("horario", mensaje);
+		
+		if(modelAndView != null && handler instanceof HandlerMethod) {
+		    modelAndView.addObject("horario", mensaje);
+		}
 	}
-	
-	
 
 }
